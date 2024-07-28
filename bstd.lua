db1ee8e15493a34994dc252f5f0642abe69e672f
nicejs-is-cool <nicejsalt@gmail.com>
2024-07-28 12:16:45 -0300

    add metatable to vec2

--- bstd.lua
+++ bstd.lua
@@ -254,10 +254,75 @@ function t.vec2.new(px, py)
 		x = px[1] or px.x
 		y = px[2] or px.y
 	end
-	return {
+	local t = {
 		x = x,
 		y = y
 	}
+	setmetatable(t, {
+		__tostring = function(self)
+			return "vec2{" .. tostring(self.x) .. ", " .. tostring(self.y) .. "}"
+		end,
+		__len = function(self) return 2 end,
+		__unm = function(self)
+			return t.vec2.new(-self.x, -self.y)
+		end,
+		__add = function(self, a) -- todo: add typechecking
+			if type(a) == "number" then
+				return t.vec2.new(self.x + a, self.y + a)
+			end
+			return t.vec2.new(self.x + a.x, self.y + a.y)
+		end,
+		__sub = function(self, a)
+			if type(a) == "number" then
+				return t.vec2.new(self.x - a, self.y - a)
+			end
+			return t.vec2.new(self.x - a.x, self.y - a.y)
+		end,
+		__mul = function(self, a)
+			if type(a) == "number" then
+				return t.vec2.new(self.x * a, self.y * a)
+			end
+			return t.vec2.new(self.x * a.x, self.y * a.y)
+		end,
+		__div = function(self, a)
+			if type(a) == "number" then
+				return t.vec2.new(self.x / a, self.y / a)
+			end
+			return t.vec2.new(self.x / a.x, self.y / a.y)
+		end,
+		__idiv = function(self, a)
+			if type(a) == "number" then
+				return t.vec2.new(self.x // a, self.y // a)
+			end
+			return t.vec2.new(self.x // a.x, self.y // a.y)
+		end,
+		__pow = function(self, a)
+			if type(a) == "number" then
+				return t.vec2.new(self.x ^ a, self.y ^ a)
+			end
+			return t.vec2.new(self.x ^ a.x, self.y ^ a.y)
+		end, -- todo: bitwise operations
+		__eq = function(self, a)
+			if type(a) == "number" then
+				return self.x == a and self.y == a
+			end
+			return self.x == a.x and self.y == a.y
+		end,
+		__lt = function(self, a)
+			if type(a) == "number" then
+				return self.x < a and self.y < a
+			end
+			return self.x < a.x and self.y < a.y
+		end,
+		__le = function(self, a)
+			if type(a) == "number" then
+				return self.x <= a and self.y <= a
+			end
+			return self.x <= a.x and self.y <= a.y
+		end
+
+	})
+	return t
 end
 --!end
 
