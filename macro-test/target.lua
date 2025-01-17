1308c5ef2430be6bf1b9cbf37ee4aed93a6b88c0
nicejs-is-cool <nicejsalt@gmail.com>
2025-01-17 12:46:11 -0300

    make it so macro.ts actually expands macros and also add nested block support

--- macro-test/target.lua
+++ macro-test/target.lua
@@ -1,8 +1,14 @@
 -- for testing macro.ts
 --!define DUCKY
+--!define DUCK
+--!define FUNNY "both!!"
 print("HELLO!")
 --!ifdef DUCK
 print("hey")
 --!end
+
+--!ifdef DUCK
+--!ifdef DUCKY
+print(FUNNY)
 --!end
---!quack
+--!end
--- macro.ts
+++ macro.ts
@@ -12,6 +12,18 @@ interface CmdContext {
 	lineIndex: number;
 	line: string;
 	filename: string;
+	data: MacroVarData;
+}
+interface MacroVarData {
+	block: {
+		index: number;
+		stack: string[];
+	}
+}
+interface MacroProcessorOutput {
+	final: string;
+	processed: string;
+	vars: DefinedMacros;
 }
 function errorMsgString(ctx: CmdContext, err: string) {
 	return `${ctx.filename}:${ctx.lineIndex+1}: ${err}`;
@@ -23,10 +35,19 @@ const macroActions: { [key: string]: (context: CmdContext, args: any[]) => any }
 	endifdef(ctx: CmdContext, args: string[]) {
 		const text = args[0];
 		const shouldInclude = ctx.infoStack.pop();
-		if (shouldInclude) {
-			ctx.addToOutput(text);
+		if (ctx.data.block.index > 1) {
+			if (shouldInclude) {
+				ctx.data.block.stack.push(text);
+			}
 		}
-		ctx.setState('normal');
+		/*if (shouldInclude) {
+			ctx.addToOutput(text);
+		}*/
+		if (--ctx.data.block.index == 0) {
+			ctx.setState('normal');
+			//console.log("blocks:", ctx.data.blockStack);
+			if (shouldInclude) ctx.addToOutput(ctx.data.block.stack.join(''));
+		}
 	}
 };
 const macroCmds: { [key: string]: (args: string[], context: CmdContext) => any } = {
@@ -36,12 +57,14 @@ const macroCmds: { [key: string]: (args: string[], context: CmdContext) => any }
 		ctx.defined[name] = restStr;
 	},
 	ifdef(args: string[], ctx: CmdContext) {
+		ctx.data.block.index++;
 		ctx.setState('blockRead')
 		ctx.infoStack.push(ctx.defined.hasOwnProperty(args[0]));
 		ctx.infoStack.push('endifdef')
 		ctx.infoStack.push('');
 	},
 	ifndef(args: string[], ctx: CmdContext) {
+		ctx.data.block.index++;
 		ctx.setState('blockRead')
 		ctx.infoStack.push(!ctx.defined.hasOwnProperty(args[0]));
 		ctx.infoStack.push('endifdef');
@@ -57,37 +80,66 @@ const macroCmds: { [key: string]: (args: string[], context: CmdContext) => any }
 				const text = ctx.infoStack.pop();
 				const action = ctx.infoStack.pop();
 				if (!macroActions.hasOwnProperty(action)) {
-					errorMsg(`Unknown stack action: ${action}`);
+					errorMsg(ctx, `Unknown stack action: ${action}`);
 					break;
 				} // its up to the macro action to return the state to 'normal'
 				macroActions[action](ctx, [text]);
 				break;
 			}
 		}
+	},
+	debug(args: string[], ctx: CmdContext) {
+		switch (args[0]) {
+			case "stack":
+				console.log(ctx.infoStack);
+				break;
+			case "ctx":
+				console.log(ctx);
+				break;
+			case "data":
+				console.log(ctx.data);
+				break;
+		}
 	}
 };
-function processMacros(fname: string, text: string, predefines: DefinedMacros | null | undefined) {
+function performMacroReplacements(text: string, defined: DefinedMacros) {
+	let output = text;
+	for (const key in defined) {
+		const val = defined[key];
+		output = output.replaceAll(key, val);
+	}
+	return output;
+}
+function processMacros(fname: string, text: string, predefines: DefinedMacros | null | undefined): MacroProcessorOutput {
 	const lines = text.split('\n');
 	const defined: DefinedMacros = predefines || {};
 	let output = "";
 	let state = "normal";
 	const infoStack: any[] = [];
+	const data: MacroVarData = {
+		block: {
+			index: 0,
+			stack: []
+		}
+	};
 	for (let i = 0; i < lines.length; i++) {
 		const line = lines[i];
+		const ctx: CmdContext = {
+			lines,
+			defined,
+			addToOutput(s: string) { output += s },
+			setState(st: string) { state = st },
+			getState() { return state },
+			infoStack,
+			lineIndex: i,
+			line,
+			filename: fname,
+			data
+		}
 		if (line.startsWith('--!')) {
 			const macroText: string = line.slice(3);
 			const [cmd, ...args] = macroText.split(' ');
-			const ctx: CmdContext = {
-				lines,
-				defined,
-				addToOutput(s: string) { output += s },
-				setState(st: string) { state = st },
-				getState() { return state },
-				infoStack,
-				lineIndex: i,
-				line,
-				filename: fname
-			}
+			
 			for (const cmdk in macroCmds) { // why tf did i do this instead of macroCmds[cmd]()
 				const cmdv = macroCmds[cmdk];
 				if (cmdk === cmd) {
@@ -114,7 +166,7 @@ function processMacros(fname: string, text: string, predefines: DefinedMacros | 
 			}
 		}
 	}
-	return output;
+	return { final: performMacroReplacements(output, defined), processed: output, vars: defined };
 }
 
 const argv = process.argv.slice(2);
@@ -139,7 +191,7 @@ for (const option of opt) {
 const fileData = await fs.readFile(file, "utf-8");
 const outputData = processMacros(file, fileData, predef);
 if (output === "-") {
-	process.stdout.write(outputData)
+	process.stdout.write(outputData.final);
 } else {
 	await fs.writeFile(output, outputData, "utf-8");
 }
